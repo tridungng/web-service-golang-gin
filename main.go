58ef72fb45dd3d40948a889b17b33e4f39b06869
bbyoda <h2trungvuong@gmail.com>
2023-11-02 17:15:53 +0100

    feat: initialise project, create album struct and endpoints to get albums

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{
+		ID: "1", Title: "Hotel California", Artist: "Eagles", Price: 25.00,
+	},
+	{
+		ID: "2", Title: "Happy Together", Artist: "The Turtles", Price: 32.25,
+	},
+	{
+		ID: "3", Title: "What A Wonderful World", Artist: "Lance Armstrong", Price: 23.75,
+	},
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		println("There is an error", err)
+	}
+
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
